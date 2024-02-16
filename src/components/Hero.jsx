5fdc767ecf1f40e4202e7d5240ba90fe42faf0c2
Erik Ly <erikly2@illinois.edu>
2024-02-16 16:25:41 -0600

    fixed bug which buttons wasnt showing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,18 +16,27 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Erik Ly</span></h1>
           <div className="flex flex-row -mt-2 gap-3 center">
             <div className="w-9 h-9">
-              <a href="https://github.com/erikly2?tab=repositories" target="_blank">
-                <img src={githubmain} alt="Button 1" />
-              </a>
+              <div onClick={() => window.open("https://github.com/erikly2?tab=repositories", "_blank")}>
+                <img 
+                  src={githubmain} 
+                  alt="github repo" 
+                />
+              </div>
             </div>
             <div className="center w-7 h-7 mt-1">
-              <a href="https://www.linkedin.com/in/erik-ly-49510b247/" target="_blank">
-                <img src={linkedin} alt="Button 2" />
-              </a>
+              <div onClick={() => window.open("https://www.linkedin.com/in/erik-ly-49510b247/", "_blank")}>
+                <img 
+                  src={linkedin} 
+                  alt="linkedin" 
+                />
+              </div>
             </div>
             <div className="center w-9 h-9">
               <a href="/src/assets/Resume - Erik.pdf" download="Resume - Erik.pdf">
-                <img src={resume} alt="Button 3" />
+                <img 
+                  src={resume} 
+                  alt="resume" 
+                />
               </a>
             </div>
           </div>
